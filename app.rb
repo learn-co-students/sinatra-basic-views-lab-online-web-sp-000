019256cb1b224dc1267179f8c82f21fe1a91c59c
Connor Cowan <ccowan003@comcast.net>
2019-11-15 14:48:50 -0800

    Done.

--- app.rb
+++ app.rb
@@ -2,12 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do
-    erb :index
-  end
-
-  get '/' do
-    "<p>97 Bowling Alley Lane, Boulder CO </p>"
-  #end
+    get '/' do
+        erb :index
+    end
 
 end
